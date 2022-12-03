710d4bc80311c58453cf3ad5874bfddcf368c26b
ManarAljadani <116364404+ManarAljadani@users.noreply.github.com>
2022-12-03 17:02:12 +0300

    Update Install_Script_Automation.sh

--- Install_Script_Automation.sh
+++ Install_Script_Automation.sh
@@ -2,7 +2,6 @@
 
 # To run script use [sudo wget https://raw.githubusercontent.com/ManarAljadani/OdooV15/16.0/Install_Script_Automation.sh]
 
-echo $(bash Install_Script_Automation.sh)
 
 echo $(sudo wget https://raw.githubusercontent.com/ManarAljadani/OdooV15/16.0/odoo_install.sh)
 echo $(sudo chmod +x odoo_install.sh)
