432fbd62b6c5dbd0cfb07f562b8074ecf8f06482
ManarAljadani <116364404+ManarAljadani@users.noreply.github.com>
2022-12-03 14:42:09 +0300

    Create Odoo-erp.sh

---
+++ Odoo-erp.sh
@@ -0,0 +1,32 @@
+
+# Odoo Enterprise install!    
+    #----------------------------------------
+    
+      sudo apt remove python3-pip
+      sudo apt-get remove python-pip-whl
+      sudo apt install python3-pip
+      #sudo reboot
+
+      sudo su $OE_USER -c "mkdir $OE_HOME/enterprise"
+      sudo su $OE_USER -c "mkdir $OE_HOME/enterprise/addons"
+
+      GITHUB_RESPONSE=$(sudo git clone --depth 1 --branch $OE_VERSION <https://www.github.com/odoo/enterprise> "$OE_HOME/enterprise/addons" 2>&1)
+
+      sudo apt-get update -y
+      #sudo reboot
+      
+      echo -e "\n---- Installing Enterprise specific libraries ----"
+      sudo -H pip3 install num2words ofxparse dbfread ebaysdk firebase_admin pyOpenSSL
+      #sudo reboot
+
+      sudo apt-get install -y python3-testresources
+      #sudo reboot
+      
+      echo -e "\n---- Added Enterprise code under $OE_HOME/enterprise/addons ----"
+      sudo npm install -g less
+      #sudo reboot
+      sudo npm install -g less-plugin-clean-css
+      #sudo reboot
+    
+    #----------------------------------------
+ 
